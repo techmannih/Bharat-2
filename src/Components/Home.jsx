492dc3f4076c350df7367b7b10eb74eb2a76c44e
techmannih <manishchaudharyttt@gmail.com>
2023-07-05 18:18:46 +0530

    change

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,4 +1,4 @@
-import React,{useCallback,useState} from "react";
+import React, { useCallback, useState } from "react";
 import { useNavigate } from "react-router";
 
 const Homepage=()=>{
@@ -8,20 +8,30 @@ const Homepage=()=>{
     const navigate=useNavigate();
 
    const handleJoin= useCallback (()=>{
+
+   if (value===undefined){
+    alert("please enter room code")}
+    else{
       navigate(`/room/${value}`);
-
-
+   
+    }
    },[navigate,value]);
    
     return (
-        <div>
+        <div className="container justify-content-center align-item-center">
+            <div className="room_name">
+                <h3>welcome to veko</h3> 
+        <div className="room">
   <input  value={value} 
   onChange={(e)=>setValue(e.target.value)} 
-  type="text" placeholder="Enter your room code"/>
-     <button  onClick={handleJoin}>Join</button>
-
-        </div>
+  type="text" placeholder="Enter your room code" required important="true"/>
+     <button  onClick={handleJoin}className="btn btn-primary position-relative text-wrap">Join</button>
+</div>
+</div>
+</div>
     )
+
+    
 }
 
-export default Homepage
+export default Homepage 
